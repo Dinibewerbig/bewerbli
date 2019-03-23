880769a311bad553c8d746db6b38aa054dca70ee
bewerbli <45333475+Dinibewerbig@users.noreply.github.com>
2019-03-23 19:12:41 +0000

    big

---
+++ assets/berufe.js
@@ -0,0 +1,639 @@
+const berufe = [
+  'Abdichter/in EFZ',
+
+  'Abdichtungspraktiker/in EBA',
+
+  'Agrarpraktiker/in EBA',
+
+  'Anlagen- und Apparatebauer/in EFZ',
+
+  'Anlagenführer/in EFZ',
+
+  'Architekturmodellbauer/in EFZ',
+
+  'Assistent/in Gesundheit und Soziales EBA',
+
+  'Augenoptiker/in EFZ',
+
+  'Automatiker/in EFZ',
+
+  'Automatikmonteur/in EFZ',
+
+  'Automobil-Assistent/in EBA',
+
+  'Automobil-Fachmann/-frau EFZ',
+
+  'Automobil-Mechatroniker/in EFZ',
+
+  'Bäcker/in-Konditor/in-Confiseur/in EBA',
+
+  'Bäcker/in-Konditor/in-Confiseur/in EFZ',
+
+  'Baumaschinenmechaniker/in EFZ',
+
+  'Baupraktiker/in EBA',
+
+  'Bauwerktrenner/in EFZ',
+
+  'Bekleidungsgestalter/in EFZ',
+
+  'Bekleidungsnäher/in EBA',
+
+  'Betonwerker/in EFZ',
+
+  'Boden-Parkettleger/in EFZ',
+
+  'Bootbauer/in EFZ',
+
+  'Bootfachwart/in EFZ',
+
+  'Buchhändler/in EFZ',
+
+  'Büchsenmacher/in EFZ',
+
+  'Bühnentänzer/in EFZ',
+
+  'Büroassistent/in EBA',
+
+  'Carrosserielackierer/in EFZ',
+
+  'Carrosseriespengler/in EFZ',
+
+  'Chemie- und Pharmapraktiker/in EBA',
+
+  'Chemie- und Pharmatechnologe/-login EFZ',
+
+  'Coiffeur/-euse EBA',
+
+  'Coiffeur/-euse EFZ',
+
+  'Dachdecker/in EFZ',
+
+  'Dachdeckerpraktiker/in EBA',
+
+  'Décolleteur/Décolleteuse',
+
+  'Dekorationsnäher/in EBA',
+
+  'Dentalassistent/in EFZ',
+
+  'Detailhandelsassistent/in EBA',
+
+  'Detailhandelsassistent/in EBA Autoteile-Logistik',
+
+  'Detailhandelsassistent/in EBA Bäckerei/Konditorei/Confiserie',
+
+  'Detailhandelsassistent/in EBA Consumer-Electronics',
+
+  'Detailhandelsassistent/in EBA Do-it-yourself',
+
+  'Detailhandelsassistent/in EBA Eisenwaren',
+
+  'Detailhandelsassistent/in EBA Elektrofach',
+
+  'Detailhandelsassistent/in EBA Farben',
+
+  'Detailhandelsassistent/in EBA Fleischwirtschaft',
+
+  'Detailhandelsassistent/in EBA Flower',
+
+  'Detailhandelsassistent/in EBA Garden',
+
+  'Detailhandelsassistent/in EBA Haushalt',
+
+  'Detailhandelsassistent/in EBA Kiosk',
+
+  'Detailhandelsassistent/in EBA Landi',
+
+  'Detailhandelsassistent/in EBA Lederwaren und Reiseartikel',
+
+  'Detailhandelsassistent/in EBA Möbel',
+
+  'Detailhandelsassistent/in EBA Musikinstrumente',
+
+  'Detailhandelsassistent/in EBA Nahrungs- und Genussmittel',
+
+  'Detailhandelsassistent/in EBA Papeterie',
+
+  'Detailhandelsassistent/in EBA Parfümerie',
+
+  'Detailhandelsassistent/in EBA Post',
+
+  'Detailhandelsassistent/in EBA Schuhe',
+
+  'Detailhandelsassistent/in EBA Spielwaren',
+
+  'Detailhandelsassistent/in EBA Sportartikel',
+
+  'Detailhandelsassistent/in EBA Teppich und Bodenbeläge',
+
+  'Detailhandelsassistent/in EBA Textil',
+
+  'Detailhandelsassistent/in EBA Uhren-Schmuck-Edelsteine',
+
+  'Detailhandelsassistent/in EBA Zoofachhandel',
+
+  'Detailhandelsfachmann/-fachfrau EFZ (Consumer-Electronics)',
+
+  'Detailhandelsfachmann/-frau EFZ',
+
+  'Detailhandelsfachmann/-frau EFZ Autoteile-Logistik',
+
+  'Detailhandelsfachmann/-frau EFZ Bäckerei/Konditorei/Confiserie',
+
+  'Detailhandelsfachmann/-frau EFZ Do-it-yourself',
+
+  'Detailhandelsfachmann/-frau EFZ Eisenwaren',
+
+  'Detailhandelsfachmann/-frau EFZ Elektrofach',
+
+  'Detailhandelsfachmann/-frau EFZ Farben',
+
+  'Detailhandelsfachmann/-frau EFZ Fleischwirtschaft',
+
+  'Detailhandelsfachmann/-frau EFZ Flower',
+
+  'Detailhandelsfachmann/-frau EFZ Garden',
+
+  'Detailhandelsfachmann/-frau EFZ Haushalt',
+
+  'Detailhandelsfachmann/-frau EFZ Kiosk',
+
+  'Detailhandelsfachmann/-frau EFZ Landi',
+
+  'Detailhandelsfachmann/-frau EFZ Lederwaren u. Reiseartikel',
+
+  'Detailhandelsfachmann/-frau EFZ Möbel',
+
+  'Detailhandelsfachmann/-frau EFZ Musikinstrumente',
+
+  'Detailhandelsfachmann/-frau EFZ Nahrungs- und Genussmittel',
+
+  'Detailhandelsfachmann/-frau EFZ öffentlicher Verkehr',
+
+  'Detailhandelsfachmann/-frau EFZ Papeterie',
+
+  'Detailhandelsfachmann/-frau EFZ Parfümerie',
+
+  'Detailhandelsfachmann/-frau EFZ Post',
+
+  'Detailhandelsfachmann/-frau EFZ Schuhe',
+
+  'Detailhandelsfachmann/-frau EFZ Spielwaren',
+
+  'Detailhandelsfachmann/-frau EFZ Sportartikel',
+
+  'Detailhandelsfachmann/-frau EFZ Teppich und Bodenbeläge',
+
+  'Detailhandelsfachmann/-frau EFZ Textil',
+
+  'Detailhandelsfachmann/-frau EFZ Uhren-Schmuck-Edelsteine',
+
+  'Detailhandelsfachmann/-frau EFZ Zoofachhandel',
+
+  'Diätkoch/-köchin EFZ',
+
+  'Drogist/in EFZ',
+
+  'Drucktechnologe/-login EFZ',
+
+  'Elektroinstallateur/in EFZ',
+
+  'Elektroniker/in EFZ',
+
+  'Elektroplaner/in EFZ',
+
+  'Entwässerungspraktiker/in EBA',
+
+  'Entwässerungstechnologe/-login EFZ',
+
+  'Fachmann/-frau Betreuung EFZ',
+
+  'Fachmann/-frau Betriebsunterhalt EFZ',
+
+  'Fachmann/-frau Bewegungs-/Gesundheitsförd. EFZ',
+
+  'Fachmann/-frau Gesundheit EFZ',
+
+  'Fachmann/-frau Hauswirtschaft EFZ',
+
+  'Fachmann/-frau Information u. Dokumentation EFZ',
+
+  'Fachmann/-frau Kundendialog EFZ',
+
+  'Fachmann/-frau Leder und Textil EFZ',
+
+  'Fachmann/-frau öffentlicher Verkehr EFZ',
+
+  'Fachmann/-frau Textilpflege EFZ',
+
+  'Fahrradmechaniker/in EFZ',
+
+  'Fahrzeugschlosser/in EFZ',
+
+  'Fassadenbauer/in EFZ',
+
+  'Fassadenbaupraktiker/in EBA',
+
+  'Feinwerkoptiker/in EFZ',
+
+  'Fleischfachassistent/in EBA',
+
+  'Fleischfachmann/-frau EFZ',
+
+  'Flexodrucker/in EFZ',
+
+  'Florist/in EBA',
+
+  'Florist/in EFZ',
+
+  'Formenbauer/in EFZ',
+
+  'Formenpraktiker/in EBA',
+
+  'Forstpraktiker/in EBA',
+
+  'Forstwart/in EFZ',
+
+  'Fotofachmann/-frau EFZ',
+
+  'Fotograf/in EFZ',
+
+  'Gärtner/in EBA',
+
+  'Gärtner/in EFZ',
+
+  'Gästeempfangsfachmann/-frau HGA (Hotel, Gastronomie, Animation)',
+
+  'Gebäudereiniger/in EBA',
+
+  'Gebäudereiniger/in EFZ',
+
+  'Gebäudetechnikplaner/in Heizung EFZ',
+
+  'Gebäudetechnikplaner/in Lüftung EFZ',
+
+  'Gebäudetechnikplaner/in Sanitär EFZ',
+
+  'Geflügelfachmann/-frau EFZ',
+
+  'Geigenbauer/in EFZ',
+
+  'Gemüsegärtner/in EFZ',
+
+  'Geomatiker/in EFZ',
+
+  'Gerüstbauer/in EFZ',
+
+  'Gerüstbaupraktiker/in EBA',
+
+  'Gestalter/in Werbetechnik EFZ',
+
+  'Gewebegestalter/in EFZ',
+
+  'Gipser/in-Trockenbauer/in EFZ',
+
+  'Gipserpraktiker/in EBA',
+
+  'Glasapparatebauer/in (EFZ/BS)',
+
+  'Glaser/in EFZ',
+
+  'Glasmaler/in EFZ',
+
+  'Gleisbauer/in EFZ',
+
+  'Gleisbaupraktiker/in EBA',
+
+  'Goldschmied/in EFZ',
+
+  'Grafiker/in EFZ',
+
+  'Graveur/in EFZ',
+
+  'Grundbauer/in EFZ',
+
+  'Grundbaupraktiker/in EBA',
+
+  'Gussformer/in EFZ',
+
+  'Gusstechnologe/-login EFZ',
+
+  'Haustechnikpraktiker/in EBA',
+
+  'Hauswirtschaftspraktiker/in EBA',
+
+  'Heizungsinstallateur/in EFZ',
+
+  'Holzbearbeiter/in EBA',
+
+  'Holzbildhauer/in EFZ',
+
+  'Holzhandwerker/in EFZ',
+
+  'Hörsystemakustiker/in EFZ',
+
+  'Hotel- und Gastrofachmann/-frau',
+
+  'Hotelfachmann/-frau EFZ',
+
+  'Hotel-Kommunikationsfachmann/-frau EFZ',
+
+  'Hotellerieangestellte/r EBA',
+
+  'Hufschmied/in EFZ',
+
+  'ICT-Fachmann/-frau EFZ',
+
+  'Industrie- und Unterlagsbodenbauer/in EFZ',
+
+  'Industrie- und Unterlagsbodenbaupraktiker/in EBA',
+
+  'Industriekeramiker/in EFZ',
+
+  'Industrielackierer/in EFZ',
+
+  'Industriepolsterer/-polsterin EFZ',
+
+  'Informatiker/in EFZ',
+
+  'Innendekorateur/in',
+
+  'Interactive Media Designer EFZ',
+
+  'Isolierspengler/in EFZ',
+
+  'Kältemontage-Praktiker/in EBA',
+
+  'Kältesystem-Monteur/in EFZ',
+
+  'Kältesystem-Planer/in EFZ',
+
+  'Kaminfeger/in EFZ',
+
+  'Kaufmann/-frau EFZ',
+
+  'Kaufmann/-frau EFZ Automobil-Gewerbe',
+
+  'Kaufmann/-frau EFZ Bank',
+
+  'Kaufmann/-frau EFZ Bauen und Wohnen',
+
+  'Kaufmann/-frau EFZ Bundesverwaltung',
+
+  'Kaufmann/-frau EFZ Chemie',
+
+  'Kaufmann/-frau EFZ Dienstleistung und Administration',
+
+  'Kaufmann/-frau EFZ Handel',
+
+  'Kaufmann/-frau EFZ Hotel-Gastro-Tourismus HGT',
+
+  'Kaufmann/-frau EFZ Internationale Speditionslogistik',
+
+  'Kaufmann/-frau EFZ Marketing & Kommunikation',
+
+  'Kaufmann/-frau EFZ Maschinen-, Elektro-, Metallind.',
+
+  'Kaufmann/-frau EFZ Nahrungsmittel-Industrie',
+
+  'Kaufmann/-frau EFZ Notariate Schweiz',
+
+  'Kaufmann/-frau EFZ Öffentliche Verwaltung',
+
+  'Kaufmann/-frau EFZ Öffentlicher Verkehr',
+
+  'Kaufmann/-frau EFZ Privatversicherung',
+
+  'Kaufmann/-frau EFZ Reisebüro',
+
+  'Kaufmann/-frau EFZ santésuisse',
+
+  'Kaufmann/-frau EFZ Spitäler/Kliniken/Heime',
+
+  'Kaufmann/-frau EFZ Transport',
+
+  'Kaufmann/-frau EFZ Treuhand/Immobilien',
+
+  'Keramiker/in EFZ',
+
+  'Kleinmotorrad- und Fahrradmechaniker/in EFZ',
+
+  'Koch/Köchin EFZ',
+
+  'Konstrukteur/in EFZ',
+
+  'Korb- und Flechtwerkgestalter/in EFZ',
+
+  'Kosmetiker/in EFZ',
+
+  'Küchenangestellte/r EBA',
+
+  'Küfer/in EFZ',
+
+  'Kunststofftechnologe/-login EFZ',
+
+  'Kunststoffverarbeiter/in EBA',
+
+  'Laborant/in EFZ',
+
+  'Lackierassistent/in EBA',
+
+  'Landmaschinenmechaniker/in EFZ',
+
+  'Landwirt/in EFZ',
+
+  'Lebensmittelpraktiker/in EBA',
+
+  'Lebensmitteltechnologe/-login EFZ',
+
+  'Logistiker/in EBA',
+
+  'Logistiker/in EFZ',
+
+  'Lüftungsanlagenbauer/in EFZ',
+
+  'Maler/in EFZ',
+
+  'Malerpraktiker/in EBA',
+
+  'Marmorist/in EFZ',
+
+  'Matrose/Matrosin der Binnenschifffahrt EFZ',
+
+  'Maurer/in EFZ',
+
+  'Mechanikpraktiker/in EBA',
+
+  'Mechanikpraktiker/in EBA, Schwerpunktausbildung Werkstoffe aus Glas und ähnlichen Werkstoffen bearbeiten',
+
+  'Mediamatiker/in EFZ',
+
+  'Medizinische/r Praxisassistent/in EFZ',
+
+  'Medizinproduktetechnologe/-login EFZ',
+
+  'Messerschmied/in EFZ',
+
+  'Metallbauer/in EFZ',
+
+  'Metallbaukonstrukteur/in EFZ',
+
+  'Metallbaupraktiker/in EBA',
+
+  'Mikromechaniker/in EFZ',
+
+  'Mikrozeichner/in EFZ',
+
+  'Milchpraktiker/in EBA',
+
+  'Milchtechnologe/-login EFZ',
+
+  'Montage-Elektriker/in EFZ',
+
+  'Motorgerätemechaniker/in EFZ',
+
+  'Motorradmechaniker/in EFZ',
+
+  'Müller/in EFZ',
+
+  'Multimediaelektroniker/in EFZ',
+
+  'Musikinstrumentenbauer/in EFZ',
+
+  'Netzelektriker/in EFZ',
+
+  'Oberflächenbeschichter/in EFZ',
+
+  'Oberflächenpraktiker/in EBA',
+
+  'Oberflächenveredler/in Uhren und Schmuck EFZ',
+
+  'Obstfachmann/-frau EFZ',
+
+  'Ofenbauer/in EFZ',
+
+  'Orthopädieschuhmacher/in EFZ',
+
+  'Orthopädist/in EFZ',
+
+  'Papiertechnologe/-login EFZ',
+
+  'Pferdefachmann/-frau EFZ',
+
+  'Pferdewart/in EBA',
+
+  'Pflästerer/Pflästerin EFZ',
+
+  'Pharma-Assistent/in EFZ',
+
+  'Physiklaborant/in EFZ',
+
+  'Plattenleger/in EFZ',
+
+  'Plattenlegerpraktiker/in EBA',
+
+  'Podologe/-login EFZ',
+
+  'Polisseur/-euse EBA',
+
+  'Polydesigner/in 3D EFZ',
+
+  'Polygraf/in EFZ',
+
+  'Polymechaniker/in EFZ',
+
+  'Printmedienpraktiker/in EBA',
+
+  'Printmedienverarbeiter/in EFZ',
+
+  'Produktionsmechaniker/in EFZ',
+
+  'Recyclist/in EFZ',
+
+  'Reifenpraktiker/in EBA',
+
+  'Restaurationsangestellte/r EBA',
+
+  'Restaurationsfachmann/-frau EFZ',
+
+  'Säger/in Holzindustrie EFZ',
+
+  'Sanitärinstallateur/in EFZ',
+
+  'Schreiner/in EFZ',
+
+  'Schreinerpraktiker/in EBA',
+
+  'Schuhmacher/in EFZ',
+
+  'Seilbahner/in EBA',
+
+  'Seilbahn-Mechatroniker/in EFZ',
+
+  'Spengler/in EFZ',
+
+  'Steinbildhauer/in EFZ',
+
+  'Steinmetz/in EFZ',
+
+  'Steinsetzer/in EBA',
+
+  'Steinwerker/in EFZ',
+
+  'Storenmontagepraktiker/in EBA',
+
+  'Storenmonteur/in EFZ',
+
+  'Strassenbauer/in EFZ',
+
+  'Strassenbaupraktiker/in EBA',
+
+  'Strassentransportfachmann/-frau EFZ',
+
+  'Strassentransportpraktiker/in EBA',
+
+  'Systemgastronomiefachmann/-frau EFZ',
+
+  'Telematiker/in EFZ',
+
+  'Textilpraktiker/in EBA',
+
+  'Textiltechnologe/-login EFZ',
+
+  'Theatermaler/in EFZ',
+
+  'Tiermedizinische/r Praxisassistent/in EFZ',
+
+  'Tierpfleger/in EFZ',
+
+  'Uhrenarbeiter/in EBA',
+
+  'Uhrmacher/in EFZ',
+
+  'Uhrmacher/in Produktion EFZ',
+
+  'Unterhaltspraktiker/in EBA',
+
+  'Veranstaltungsfachmann/-frau EFZ',
+
+  'Vergolder/in-Einrahmer/in EFZ',
+
+  'Verpackungstechnologe/-login EFZ',
+
+  'Weintechnologe/-login EFZ',
+
+  'Werkstofftechniker/in FZ Werkstoffprüfung/ Werkstoffprüfung und Wärmebehandlung (Liechtensteinisches Fähigkeitszeugnis)',
+
+  'Winzer/in EFZ',
+
+  'Wohntextilgestalter/in EFZ',
+
+  'Zahntechniker/in EFZ',
+
+  'Zeichner/in EFZ',
+
+  'Zimmermann/Zimmerin EFZ'
+]
+
+export default berufe
---
+++ assets/steps.js
@@ -0,0 +1,229 @@
+const steps = [
+  {
+    frage: 'Wähle den Beruf?',
+    component: 'Form0',
+    options: []
+  },
+  {
+    frage: 'Wähle den Beruf?',
+    component: 'Form1',
+    options: ['hello12', 'naturlich', 'weiterso']
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  },
+  {
+    frage: 'Wie geht es weiter?',
+    component: 'Form2',
+    options: []
+  }
+]
+
+export default steps
